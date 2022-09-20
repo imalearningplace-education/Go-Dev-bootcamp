0ae720cd49f8d0bbf3d05fe7e124bd9ee9f5035f
hgrafa <hugorafell@gmail.com>
2022-09-20 18:05:13 -0300

    Adicionando Exemplos da aula 01 - Introdução ao C#

---
+++ Exemplos/Base/Base/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Base {
+
+    public class Program {
+
+        public static void Main() {
+
+            string nome = "Joao da Silva";
+            int idade = 20;
+            double altura = 1.75;
+            bool estaChovendo = false;
+
+            // concatenação
+            Console.WriteLine("Oi, meu nome é " + nome + " minha idade é " + idade);
+            Console.WriteLine("Tenho " + altura + "m");
+
+            // interpolação
+            Console.WriteLine($"Oi, meu nome é {nome} minha idade é {idade}");
+            Console.WriteLine($"Tenho {altura} m");
+            
+            // se
+            if ( estaChovendo == true ) {
+                Console.WriteLine("hmmm vou ficar em casa");
+            } else {
+                Console.WriteLine("vou sair");
+            }
+
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Turma \<GoDev>
+
+- Turma de C# - Ímã learning place
+- prof. **Hugo Rafael**
