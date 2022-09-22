714e86bf904327acb98177034ca506c4f6c3bc1f
hgrafa <hugorafell@gmail.com>
2022-09-22 19:30:34 -0300

    Adicionando solução: "Aprovado ou Reprovado"

---
+++ Soluções/Autorais/AprovadoOuReprovado/AprovadoOuReprovado/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AprovadoOuReprovado {
+    public class Program {
+        public static void Main() {
+
+            double nota;
+
+            Console.Write("Digite a sua nota: ");
+            nota = double.Parse(Console.ReadLine());
+
+            if (nota >= 7.0) {
+                Console.WriteLine("Aprovado!");
+            } else if (nota >= 4.0) {
+                Console.WriteLine("Recuperação");
+            } else {
+                Console.WriteLine("Reprovado!");
+            }
+
+
+            // 4 <= nota < 7 : Recuperação
+            // < 4 : Reprovado
+
+        }
+    }
+}
