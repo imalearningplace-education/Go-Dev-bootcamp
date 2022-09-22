9ae30f05ec70da241dacc838a0e160824972c4c4
hgrafa <hugorafell@gmail.com>
2022-09-22 19:30:13 -0300

    Adicionando solução: "Fliper"

---
+++ Soluções/Olímpiadas/Flipper/Flipper/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Flipper {
+
+    public class Program {
+
+        public static void Main() {
+            
+            int p, r;
+
+            Console.Write("Digite o valor da portinha P: ");
+            p = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor da portinha R: ");
+            r = int.Parse(Console.ReadLine());
+
+            if( p == 0 ) {
+                Console.WriteLine("C");
+            } else if( r == 0 ) {
+                Console.WriteLine("B");
+            } else {
+                Console.WriteLine("A");
+            }
+
+        }
+
+    }
+}
