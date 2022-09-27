939e9a09b2543485bd92ed2ec0c90028b15c0517
hgrafa <hugorafell@gmail.com>
2022-09-27 08:01:15 -0300

    Adicionando: Exemplo para vetores

---
+++ Exemplos/Vetores/Vetores/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Vetores {
+
+    public class Program {
+
+        public static void Main () {
+
+            // vetor: bloco contiguo de memória
+            int[] sequencia = new int[12];
+            // n elementos: posições 0 -> n-1
+
+            sequencia[0] = 6;
+            sequencia[1] = 5;
+            sequencia[2] = 4;
+            sequencia[3] = 3;
+
+            for (int i = 0; i < sequencia.Length; i++) { 
+                Console.WriteLine($"elemento {i} : {sequencia[i]}");
+            }
+
+            Console.WriteLine("------------------");
+
+        }
+
+    }
+
+}
