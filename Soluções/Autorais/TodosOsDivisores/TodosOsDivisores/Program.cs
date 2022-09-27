57fb5afd6dd0fca8d36b617ff3687fd2593daf48
hgrafa <hugorafell@gmail.com>
2022-09-27 08:01:44 -0300

    Adicionando: Solução para 'Todos os divisores'

---
+++ Soluções/Autorais/TodosOsDivisores/TodosOsDivisores/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TodosOsDivisores {
+
+    public class Program {
+
+        public static void Main() {
+
+            // divisores positivos
+            int n = int.Parse(Console.ReadLine());
+            bool first = true;
+
+            for (int i = 1; i <= n; i++) {
+
+                if (n % i == 0) {
+                    if ( first ) {
+                        Console.WriteLine(i);
+                        first = false;
+                    } else {
+                        Console.Write(" " + i);
+                    }
+                }
+    
+            }
+        }
+    }
+}
