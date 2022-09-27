cd14a30c826607ca4b5a91dab59eb0a2025a9ead
hgrafa <hugorafell@gmail.com>
2022-09-27 08:00:28 -0300

    Adicionando exemplos para entradas em uma linha

---
+++ Exemplos/EntradasUmaLinha/EntradasUmaLinha/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EntradasUmaLinha {
+
+    public class Program {
+
+        public static void Main() {
+
+            string[] linha = Console.ReadLine().Split(" ");
+         
+            // realizar leitura
+
+            // atribuir as variaveis
+            string nome = linha[0];
+            int idade = int.Parse(linha[1]);
+            double altura = double.Parse(linha[2]);
+
+            // regras de negocio
+            for (int i = 0; i < linha.Length; i++) {
+                Console.WriteLine(linha[i] );
+            }
+
+            Console.WriteLine($"Nome: {nome} | Idade: {idade} | Altura: {altura}");
+
+        }
+
+    }
+
+}
