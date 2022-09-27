9de6199bb797e8da9f9c0195e20caa5770096c7e
hgrafa <hugorafell@gmail.com>
2022-09-27 08:01:02 -0300

    Adicionando: Exemplo para repetição

---
+++ Exemplos/Repeticoes2/Repeticoes2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Repeticoes2 {
+
+    public class Program {
+
+        public static void Main() {  
+
+            for (int i = 0; i < 10; i++) {
+
+                if (i % 2 == 0) {
+                    Console.WriteLine("devemos pular esse...");
+                    continue;
+                }
+
+                if (i == 8) {
+                    Console.WriteLine("Valor nao permitido!");
+                    break;
+                }
+
+                Console.WriteLine($"pressao = {i}");
+            }
+
+            Console.WriteLine("Fim do programa");
+
+        }
+
+    }
+
+}
