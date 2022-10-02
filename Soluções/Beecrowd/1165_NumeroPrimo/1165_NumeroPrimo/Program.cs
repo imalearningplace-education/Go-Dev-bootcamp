40206d80eb4ee84389813210260f3b27ca853db0
hgrafa <hugorafell@gmail.com>
2022-10-02 09:23:37 -0300

    Adicionando: Beecrowd 1165

---
+++ Soluções/Beecrowd/1165_NumeroPrimo/1165_NumeroPrimo/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace NumeroPrimo_1165 { 
+    
+    public class Program {
+
+        static bool isPrime(int x) {
+
+            if (x == 1) {
+                return false;
+            }
+
+            for (int i = 2; i < x; i++) {
+                if (x % i == 0 ) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static void Main () {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) {
+
+                int x = int.Parse(Console.ReadLine());
+
+                string resposta = isPrime(x) ? $"{x} " : $"{x} nao ";
+                resposta += "eh primo";
+
+                Console.WriteLine(resposta);
+            }
+
+        }
+    }
+
+}
