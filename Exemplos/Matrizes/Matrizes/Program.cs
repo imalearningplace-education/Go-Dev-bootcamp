60435e91193b1c5130a7accd6f47066479878410
hgrafa <hugorafell@gmail.com>
2022-10-05 11:58:55 -0300

    Adicionando: Exemplo Matrizes

---
+++ Exemplos/Matrizes/Matrizes/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Matrizes {
+
+    public class Program {
+
+        public static void Main() {
+
+            int[,] array2d = new int[3, 3];
+
+            for(int i = 0; i < 3; i++) {
+                array2d[0, i] = i;
+                array2d[1, i] = i;
+                array2d[2, i] = i;
+            }
+
+            for(int i=0; i<3; i++) {
+                for(int j=0; j<3; j++) {
+                    Console.Write($"{array2d[j, i]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+
+}
