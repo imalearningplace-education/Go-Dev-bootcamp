c41a2573d994c224a8f0ba523a88a8929525d227
hgrafa <hugorafell@gmail.com>
2022-10-05 11:58:23 -0300

    Adicionando: Beecrowd 2399

---
+++ Soluções/Beecrowd/2399_CampoMinado/2399_CampoMinado/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace CampoMinado_2399 {
+
+    public class Program { 
+        
+        static bool EstaNaPrimeiraPosicao(int posicao) {
+            if (posicao == 0) {
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool EstaNaUltimaPosicao(int posicao, int tamanho) {
+            if (posicao == tamanho - 1) {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void Main() {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[] minas = new int[n];
+            int[] numeroBombas = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                minas[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++) {
+                int anterior = (!EstaNaPrimeiraPosicao(i)) ? minas[i - 1] : 0 ;
+                int posterior = (!EstaNaUltimaPosicao(i,n)) ? minas[i + 1] : 0;
+
+                int atual = minas[i];
+
+                numeroBombas[i] = anterior + atual + posterior;
+                Console.WriteLine( numeroBombas[i] );
+            }
+
+        }
+
+    }
+}
