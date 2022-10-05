d5d6463ed6a0bff62c085f5601b8bd6ac39364e9
hgrafa <hugorafell@gmail.com>
2022-10-05 11:57:49 -0300

    Adicionando: Beecrowd 1180

---
+++ Soluções/Beecrowd/1180_MenorEPosicao/1180_MenorEPosicao/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MenorEPosicao_1180{
+
+    public class Program {
+
+        public static void Main() {
+
+            int n = int.Parse(Console.ReadLine());
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            // suposto (e com certeza falso) menor
+            int posMenor = 0;
+
+            for(int i = 1; i < n; i++) {
+
+                int valorAtual = int.Parse(valores[i]);
+                int valorMenor = int.Parse(valores[posMenor]);
+
+                if (valorAtual < valorMenor) {
+                    posMenor = i;
+                }
+
+            }
+
+            Console.WriteLine($"Menor valor: {valores[posMenor]}");
+            Console.WriteLine($"Posicao: {posMenor}");
+
+        }
+
+    }
+
+}
