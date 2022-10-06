c5b60aec6e69392e6eb70e7a7905f4521ad8956b
hgrafa <hugorafell@gmail.com>
2022-10-06 15:59:23 -0300

    Adicionando: Solução Projeto Banco

---
+++ Soluções/Autorais/Banco/Banco/Entities/Conta.cs
@@ -0,0 +1,53 @@
+﻿namespace Banco.Entities {
+    
+    public class Conta {
+
+        // atributos
+        public string ID;
+        public string Nome;
+        public double Saldo;
+
+        // construtor e sobrecargas
+        public Conta(string id, string nome, double saldo) {
+            ID = id;
+            Nome = nome;
+            Saldo = saldo;
+        }
+
+        public Conta(string iD, string nome) {
+            ID = iD;
+            Nome = nome;
+            Saldo = 0.0;
+        }
+
+        // métodos
+        // 1. 
+        public void Depositar(double quantia) {
+            Saldo += quantia;
+        }
+
+        // 2. sacar
+        public bool Sacar(double quantia) {
+
+            if (Saldo >= quantia) {
+                Saldo -= quantia;
+                return true;
+            }
+
+            return false;
+        }
+
+        // 3. transferir
+        public bool Transferir(double quantia, Conta destino) {
+            if(Saldo >= quantia) {
+                Saldo -= quantia;
+                destino.Saldo += quantia;
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+
+}
---
+++ Soluções/Autorais/Banco/Banco/Program.cs
@@ -0,0 +1,24 @@
+﻿using Banco.Entities;
+
+namespace Banco {
+
+    public class Program {
+
+        public static void Main() {
+
+            Conta acc1 = new Conta("123A", "Pedro", 200.0);
+            Conta acc2 = new Conta("123B", "Joao");
+
+            acc1.Sacar(175.0);
+            acc1.Sacar(100.0);
+            acc1.Depositar(200.0);
+            acc1.Transferir(150.0, acc2);
+
+            Console.WriteLine($"Saldo final conta do(a) {acc1.Nome} = R${acc1.Saldo:F2}");
+            Console.WriteLine($"Saldo final conta do(a) {acc2.Nome} = R${acc2.Saldo:F2}");
+
+        }
+
+    }
+
+}
