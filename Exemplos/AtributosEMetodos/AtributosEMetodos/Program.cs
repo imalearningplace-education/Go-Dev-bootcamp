4a6ba0faa04a68bc33b6643b98dac5b59a346bde
hgrafa <hugorafell@gmail.com>
2022-10-06 15:58:44 -0300

    Adicionando: Exemplo "Atributos e Métodos"

---
+++ Exemplos/AtributosEMetodos/AtributosEMetodos/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace AtributosEMetodos {
+
+    class Pessoa {
+
+        // atributos
+        public string Nome;
+        public int Idade;
+        public double Altura;
+
+        // métodos
+        public void DizerOi() {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void FazerAniversario() {
+            Idade++;
+        }
+
+    }
+
+    public class Program {
+
+        public static void Main() {
+
+            Pessoa joao = new Pessoa();
+
+            joao.Nome = "Joao da Silva";
+            joao.Idade = 23;
+            joao.Altura = 1.9;
+
+            joao.DizerOi();
+            joao.FazerAniversario();
+
+            Console.WriteLine($"Idade = {joao.Idade}");
+           
+        }
+
+    }
+
+}
