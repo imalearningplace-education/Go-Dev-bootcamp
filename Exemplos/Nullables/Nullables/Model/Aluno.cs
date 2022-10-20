f960df1219ce1f8e05dd29f3e6ca44a8c55a16b7
hgrafa <hugorafell@gmail.com>
2022-10-20 17:14:52 -0300

    Adicionando: Exemplo "nullables"

---
+++ Exemplos/Nullables/Nullables/Model/Aluno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullables.Model {
+    public class Aluno {
+ 
+        public string Nome { get; set; }
+
+        // criação de um tipo nullable
+        public int? Nota { get; set; }
+
+        public Aluno(string? nome) {
+            // checando se o argumento é nulo
+            // se sim => Nome = "undefined"
+            // senão => Nome = nome
+            Nome = nome ?? "undefined";
+        }
+
+        public void FazerProva(int nota) {
+            // Nota = Nota ?? nota
+            Nota ??= nota;
+        }
+
+        public double Soma(int a, int b) => a + b;
+
+    }
+}
---
+++ Exemplos/Nullables/Nullables/Program.cs
@@ -0,0 +1,26 @@
+﻿using Nullables.Model;
+
+namespace Nullables {
+
+    public class Program {
+
+        public static void Main() {
+
+            Aluno pedro = new(null);
+
+            Console.WriteLine(pedro.Nome);
+            Console.WriteLine(pedro.Nota);
+
+            pedro.FazerProva(6);
+
+            Console.WriteLine(pedro.Nota);
+
+            pedro.FazerProva(9);
+
+            Console.WriteLine(pedro.Nota);
+            
+        }
+
+    }
+
+}
