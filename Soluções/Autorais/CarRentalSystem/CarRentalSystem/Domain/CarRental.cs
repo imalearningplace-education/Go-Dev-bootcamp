1c724b9319fb8836d74945527ed9acf7ddfc9231
hgrafa <hugorafell@gmail.com>
2022-10-20 19:33:17 -0300

    Adding Car Rental project

---
+++ Soluções/Autorais/CarRentalSystem/CarRentalSystem/Domain/CarRental.cs
@@ -0,0 +1,17 @@
+﻿namespace CarRentalSystem.Entity {
+    public class CarRental {
+
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle) {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+        }
+
+    }
+}
---
+++ Soluções/Autorais/CarRentalSystem/CarRentalSystem/Domain/Invoice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalSystem.Entity {
+    public class Invoice {
+
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public double TotalPayment {
+            get {
+                return BasicPayment + Tax;
+            }
+        }
+
+        public Invoice(double basicPayment, double tax) {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+    }
+}
