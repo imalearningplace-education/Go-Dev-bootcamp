225ce91a9a179b970c9a08a09ebd954b7fb9e70d
hgrafa <hugorafell@gmail.com>
2022-10-20 19:39:00 -0300

    Adicionando exemplo: "Vetores Dinâmicos"

---
+++ Soluções/Autorais/VetoresDinamicos/VetoresDinamicos/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace VetoresDinamicos {
+
+    public class Program {
+
+        public static void Main() {
+
+/*            
+ *          VetorDinamico vetor = new VetorDinamico();
+
+            Console.WriteLine(vetor.Valores.Length);
+
+            vetor.AdicionarNoFinal(3);
+
+            Console.WriteLine(vetor.Valores.Length);
+
+            vetor.AdicionarNoFinal(2);
+            vetor.AdicionarNoFinal(1);
+            vetor.AdicionarNoFinal(8);
+            vetor.AdicionarNoFinal(9);
+
+            foreach(int valor in vetor.Valores) {
+                Console.WriteLine(valor);
+            }
+*/
+
+           List<int> valores = new List<int>();
+
+            valores.Add(2);
+            valores.Add(3);
+            valores.Add(4);
+            valores.Add(5);
+        
+        }
+
+    }
+
+}
---
+++ Soluções/Autorais/VetoresDinamicos/VetoresDinamicos/VetorDinamico.cs
@@ -0,0 +1,22 @@
+﻿namespace VetoresDinamicos {
+    public class VetorDinamico {
+
+        public int[] Valores { get; set; }
+
+        public VetorDinamico() {
+            Valores = new int[0];
+        }
+
+        public void AdicionarNoFinal(int valor) {
+            int[] copy = Valores;
+            Valores = new int[Valores.Length + 1];
+
+            for(int i = 0; i < copy.Length; i++) {
+                Valores[i] = copy[i];
+            }
+
+            Valores[Valores.Length - 1] = valor;
+        }
+
+    }
+}
