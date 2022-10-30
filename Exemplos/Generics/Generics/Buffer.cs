5b5450a78c3ed6953cc4042b8afe89cca4524a17
hgrafa <hugorafell@gmail.com>
2022-10-29 21:50:34 -0300

    Adicionando: Exemplo sobre tipos genericos

---
+++ Exemplos/Generics/Generics/Buffer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics {
+    public class Buffer <T> {
+
+        public List<T> Acumulador { get; private set; }
+
+        public Buffer() {
+            Acumulador = new List<T>();
+        }
+
+        public void Acumula(T valor) {
+            Acumulador.Add(valor);
+        }
+
+        public void Imprime() {
+            foreach (T valor in Acumulador) {
+                Console.WriteLine(valor);
+            }
+        }
+
+    }
+}
---
+++ Exemplos/Generics/Generics/BufferApplication.cs
@@ -0,0 +1,17 @@
+﻿namespace Generics {
+
+    public class BufferApplication {
+
+        public static void Main() {
+
+            Buffer<int> meuAcumulador = new Buffer<int>();
+
+            meuAcumulador.Acumula(2);
+
+            meuAcumulador.Imprime();
+
+        }
+
+    }
+
+}
