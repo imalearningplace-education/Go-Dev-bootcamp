a1219ba65c0b52512d76beaf6f315e227db52aea
hgrafa <hugorafell@gmail.com>
2022-10-29 21:50:18 -0300

    Adicionando Exemplo: Exceções no c#

---
+++ Exemplos/CoreExceptions/CoreExceptions/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CoreExceptions {
+
+    public class Program {
+
+
+        public static void Main () {
+            Divisao();
+
+        }
+
+        private static void Divisao() {
+            Console.Write("Digite o primeiro valor: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            int b = int.Parse(Console.ReadLine());
+
+            try {
+                Console.WriteLine($"O resultado da divisão é: {a / b}");
+            } catch (DivideByZeroException exceptionObj) {
+                Console.WriteLine(exceptionObj.Message);
+            }
+
+            Console.WriteLine("Finalizando o método!");
+
+        }
+    }
+
+}
