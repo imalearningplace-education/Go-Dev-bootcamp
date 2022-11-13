2b2d2ccf0af54823076df1b03db34f3645b5e305
hgrafa <hugorafell@gmail.com>
2022-11-13 14:31:58 -0300

    Adicionando Exemplo: LINQs em C#

---
+++ Exemplos/LINQ1/LINQ1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LINQ1 {
+
+    public class Program {
+
+        public static void Main () {
+
+            List<int> numbers = new List<int> { 2, 3, 4, 5, 6 };
+
+            numbers
+                .Where(n => n % 2 == 0)
+                .Select(n => 10 * n)
+                .ToList()
+                .ForEach(Console.WriteLine);
+                  
+
+        }
+
+    }
+
+}
